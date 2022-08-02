9cc5f4c1782236ae6bcd5b4132965573a3111188
Josh <joshlee0201@gmail.com>
2022-08-01 17:03:59 -0700

    finished back-end django for services

---
+++ service/api/service_rest/encoders.py
@@ -0,0 +1,48 @@
+from common.json import ModelEncoder
+from .models import Technician, AutomobileVO, ServiceAppointment
+
+class AutomovileVOEncoder(ModelEncoder):
+    model = AutomobileVO
+    properties = ['VIN']
+
+class TechnicianListEncoder(ModelEncoder):
+    model = Technician
+    properties = [
+        'name',
+        'employee_number',
+        'id',
+    ]
+
+class TechnicianDetailEncoder(ModelEncoder):
+    model = Technician
+    properties = [
+        'name',
+        'employee_number',
+    ]
+
+class ServiceAppointmentListEncoder(ModelEncoder):
+    model = ServiceAppointment
+    properties = [
+        'VIN',
+        'owner',
+        'date_time',
+        'technician',
+        'reason',
+        'id',
+    ]
+    encoders = {
+        'technician': TechnicianDetailEncoder(),
+    }
+
+class ServiceAppointmentDetailEncoder(ModelEncoder):
+    model = ServiceAppointment
+    properties = [
+        'VIN',
+        'owner',
+        'date_time',
+        'technician',
+        'reason',
+    ]
+    encoders = {
+        'technician': TechnicianDetailEncoder(),
+    }
--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class AutomobileVO(models.Model):
+    VIN = models.BigIntegerField()
+
+class Technician(models.Model):
+    name = models.CharField(max_length=30)
+    employee_number = models.PositiveBigIntegerField()
+
+    def get_api_url(self):
+        return reverse("api_technician", kwargs={"pk": self.pk})
+
+
+class ServiceAppointment(models.Model):
+    VIN = models.PositiveBigIntegerField()
+    owner = models.CharField(max_length=30)
+    date_time = models.DateTimeField()
+    technician = models.ForeignKey(
+        Technician,
+        related_name='technician',
+        on_delete=models.PROTECT
+    )
+    reason = models.TextField()
+
+    def get_api_url(self):
+        return reverse("api_service_appointment", kwargs={"pk": self.pk})
