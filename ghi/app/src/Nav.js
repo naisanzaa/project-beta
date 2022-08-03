d86f2abd2dac900001132e111bf393addacf6818
Josh <joshlee0201@gmail.com>
2022-08-03 10:37:41 -0700

    updated navbar to include all links for service microservice

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -9,7 +9,32 @@ function Nav() {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <ul className="navbar-nav me-auto mb-5 mb-lg-1">
+          <li className="nav-item">
+              <NavLink className="nav-link active" aria-current="page" to="/">
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link active" aria-current="page" to="/service-appointments/">
+                Service Appointments
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link active" aria-current="page" to="/service-appointments/new/">
+                Register an Appointment
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link active" aria-current="page" to="/service-appointments/history/">
+                Service History
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link active" aria-current="page" to="/technicians/new">
+                Register a Technician
+              </NavLink>
+            </li>
           </ul>
         </div>
       </div>
