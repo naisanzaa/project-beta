e4fa0cbee88e3a514bd89d2f8f27f91ff4028b27
Thao <thao.ng11@gmail.com>
2022-08-05 16:51:17 -0400

    Adding the README, and comments for Views

--- sales/api/sales_rest/views.py
+++ sales/api/sales_rest/views.py
@@ -11,7 +11,45 @@ from .models import  SalesStaff, Customer, Sale, AutomobileVO
 
 @require_http_methods(["GET", "POST"])
 def api_sales(request):
+    """
+    Collection RESTful API handler for sales objects
+    GET:
+    Returns a dictionary with a single key "sales"
+    {
+        "sales": [
+		{
+			"href": URL to the sale,
+			"auto": {
+				"import_href": URL of the automobile,
+				"vin": database vin for the automobile,
+			},
+			"sales_staff": {
+				"href": URL to the sales_staff,
+				"name": name of the sales_staff,
+				"employee_number": employee number for sales_staff,
+				"id": database id for the sales_staff,
+			},
+			"customer": {
+				"href": URL for the customer,
+				"name": name of the customer,
+				"address": address of the customer,
+				"phone": phone of the customer,
+				"id": database id for customer
+			},
+			"price": cost of the automobile,
+            ...
+        ]
+    }
+    POST:
+    Create a sale resourrce and return its details:
+    {
+        "auto": vin number of the automobile,
+        "sales_staff": database id for the sales_staff,
+        "customer": database id for customer,
+        "price": cost of the automobile
+        }
 
+    """
     if request.method == "GET":
         sales = Sale.objects.all()
         return JsonResponse(
@@ -51,6 +89,15 @@ def api_sales(request):
 
 @require_http_methods(["GET", "DELETE"])
 def api_sale(request, pk):
+    """
+    Single-object API for the Sale resource
+
+    GET:
+    Return the information for a Sale resource based on the value of pk
+
+    DELETE:
+    Removes the sale resource from the application
+    """
         
     if request.method == "GET":
         sale=Sale.objects.get(id=pk)
@@ -78,6 +125,31 @@ def api_sale(request, pk):
 
 @require_http_methods(["GET", "POST"])
 def api_sales_staffs(request):
+    """
+    Collection RESTful API handler for sales staffs objects
+    GET:
+    Returns a dictionary with a single key "sales_staffs"
+    {
+        "sales_staffs": [
+		{
+
+            "href": URL to the sales_staff,
+            "name": name of the sales_staff,
+            "employee_number": employee number for sales_staff,
+            "id": database id for the sales_staff,
+        },
+        
+        ...
+        ]
+    }
+    POST:
+    Create a sale staff resourrce and return its details:
+    {
+        "name": name of the sales_staff,
+        "employee_number": employee number for sales_staff,
+    }
+
+    """
     if request.method == "GET":
         sales_staffs = SalesStaff.objects.all()
         return JsonResponse(
@@ -101,7 +173,16 @@ def api_sales_staffs(request):
             return response
 
 @require_http_methods(["GET", "DELETE"])
-def api_sales_staff(request, pk):    
+def api_sales_staff(request, pk):
+    """
+    Single-object API for the SalesStaff resource
+
+    GET:
+    Return the information for a SalesStaff resource based on the value of pk
+
+    DELETE:
+    Removes the SalesStaff resource from the application
+    """    
     if request.method == "GET":
         sales_staff = SalesStaff.objects.get(id=pk)
         return JsonResponse(
@@ -128,6 +209,31 @@ def api_sales_staff(request, pk):
 
 @require_http_methods(["GET", "POST"])
 def api_customers (request):
+    """
+    Collection RESTful API handler for customers objects
+    GET:
+    Returns a dictionary with a single key "customers"
+    {
+        "customers": [
+		{
+            "href": URL for the customer,
+            "name": name of the customer,
+            "address": address of the customer,
+            "phone": phone of the customer,
+            "id": database id for customer
+		},
+        ...
+        ]
+    }
+    POST:
+    Create a customer resourrce and return its details:
+    {
+        "name": name of the customer,
+        "address":  address of the customer,
+        "phone": phone of the customer,
+    }
+
+    """
     if request.method == "GET":
         customers = Customer.objects.all()
         return JsonResponse(
@@ -152,7 +258,15 @@ def api_customers (request):
 
 @require_http_methods(["GET", "DELETE"])
 def api_customer(request, pk):
-        
+    """
+    Single-object API for the Customer resource
+
+    GET:
+    Return the information for a Customer resource based on the value of pk
+
+    DELETE:
+    Removes the Customer resource from the application
+    """     
     if request.method == "GET":
         customer=Customer.objects.get(id=pk)
         return JsonResponse(
